7171c25db95e2fd6cbd16f97339c1f7affd61521
Ivandragon003 <ivandefelice05@gmail.com>
2025-11-25 18:17:02 +0100

    debug di prova

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
-// src/config.ts
-const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://bugboard26-h7a9g8eaasd4azdv.italynorth-01.azurewebsites.net/api'
-  : process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+
+const API_BASE_URL = 
+  process.env.REACT_APP_API_URL ||  
+  'https://bugboard26-h7a9g8eaasd4azdv.italynorth-01.azurewebsites.net/api'; 
 
 export default API_BASE_URL;
