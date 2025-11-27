e39f194199f97572f97f3ef99fd8c2ebf1447101
Ivandragon003 <ivandefelice05@gmail.com>
2025-11-27 09:36:33 +0100

    risolto bugg di login

--- src/main/java/it/unina/bugboard/converter/StatoConverter.java
+++ src/main/java/it/unina/bugboard/converter/StatoConverter.java
@@ -7,28 +7,33 @@ import org.postgresql.util.PGobject;
 import java.sql.SQLException;
 
 @Converter(autoApply = true)
-public class StatoConverter implements AttributeConverter<Stato, PGobject> {
+public class StatoConverter implements AttributeConverter<Stato, Object> {
 
-	@Override
-	public PGobject convertToDatabaseColumn(Stato attribute) {
-		if (attribute == null)
-			return null;
+    @Override
+    public Object convertToDatabaseColumn(Stato attribute) {
+        if (attribute == null) return null;
+        
+        try {
+            PGobject pgObject = new PGobject();
+            pgObject.setType("stato");
+            pgObject.setValue(attribute.name());
+            return pgObject;
+        } catch (SQLException e) {
+            throw new RuntimeException("Errore conversione Stato", e);
+        }
+    }
 
-		PGobject pgObject = new PGobject();
-		pgObject.setType("stato"); 
-		try {
-			pgObject.setValue(attribute.name());
-		} catch (SQLException e) {
-			throw new RuntimeException("Errore conversione Stato", e);
-		}
-		return pgObject;
-	}
-
-	@Override
-	public Stato convertToEntityAttribute(PGobject dbData) {
-		if (dbData == null)
-			return null;
-		return Stato.valueOf(dbData.getValue());
-	}
-
+    @Override
+    public Stato convertToEntityAttribute(Object dbData) {
+        if (dbData == null) return null;
+        
+        String value;
+        if (dbData instanceof PGobject) {
+            value = ((PGobject) dbData).getValue();
+        } else {
+            value = dbData.toString();
+        }
+        
+        return Stato.valueOf(value);
+    }
 }
