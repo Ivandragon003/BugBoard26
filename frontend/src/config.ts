4fec4b58c45b47db9b54e7cd338c3c1a4c73f928
Matrbo7 <mattiacerbone7@gmail.com>
2025-11-27 16:06:33 +0100

    aggiornamenti importanti

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,4 @@
 
-const API_BASE_URL = 
-  process.env.REACT_APP_API_URL ||  
-  'https://bugboard26-h7a9g8eaasd4azdv.italynorth-01.azurewebsites.net/api'; 
+const API_BASE_URL = "http://localhost:8080/api";
 
 export default API_BASE_URL;
